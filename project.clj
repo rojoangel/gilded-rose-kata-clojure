7e86529e342e2e3041e9bb8fcf980de5daae4756
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-04-18 22:10:33 +0200

    initial code to refactor

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gilded-rose "0.1.0-SNAPSHOT"
+  :description "Gilded Rose Kata"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
