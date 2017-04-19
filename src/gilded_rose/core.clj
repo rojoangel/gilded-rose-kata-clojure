ea730bffbbaba51e745279d44b52e6221f3f9374
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-04-19 21:42:17 +0200

    remove "update-current-inventory" function

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -35,15 +35,3 @@
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
-
-(defn update-current-inventory []
-  (let [inventory
-        [
-         (item "+5 Dexterity Vest" 10 20)
-         (item "Aged Brie" 2 0)
-         (item "Elixir of the Mongoose" 5 7)
-         (item "Sulfuras, Hand Of Ragnaros" 0 80)
-         (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-         ]]
-    (update-inventory inventory)
-    ))
