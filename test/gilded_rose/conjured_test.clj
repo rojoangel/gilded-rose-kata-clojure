b7be743cc38e67e359b83860601c96cb6440ee36
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-05-06 13:54:09 +0200

    add test for conjured aged brie

--- test/gilded_rose/conjured_test.clj
+++ test/gilded_rose/conjured_test.clj
@@ -3,21 +3,31 @@
             [gilded-rose.conjured :refer :all]
             [gilded-rose.inventory :as inventory]))
 
-(def normal-conjured-item
+(def conjured-normal-item
   (conjure "Potion" 12 33))
 
-(def sell-date-passed-normal-conjured-item
+(def sell-date-passed-conjured-normal-item
   (conjure "Potion" 0 33))
+
+(def conjured-aged-brie
+  (conjure "Aged Brie" 2 0))
+
 
 (deftest
   about-conjured-item
   (testing
-    "Conjured"
+    "Conjured Normal item"
     (testing
       "Normal item degrades in quality twice as fast as normal items"
-      (let [quality (:quality normal-conjured-item)]
-        (is (= (- quality 2) (:quality (inventory/age normal-conjured-item))))))
+      (let [quality (:quality conjured-normal-item)]
+        (is (= (- quality 2) (:quality (inventory/age conjured-normal-item))))))
     (testing
       "Once the sell by date has passed, normal item quality degrades twice as fast"
-      (let [quality (:quality sell-date-passed-normal-conjured-item)]
-        (is (= (- quality 4) (:quality (inventory/age sell-date-passed-normal-conjured-item))))))))
+      (let [quality (:quality sell-date-passed-conjured-normal-item)]
+        (is (= (- quality 4) (:quality (inventory/age sell-date-passed-conjured-normal-item)))))))
+  (testing
+    "Conjured Aged Brie"
+    (testing
+      "Aged Brie increases in quality twice as fast as normal items"
+      (let [quality (:quality conjured-aged-brie)]
+        (is (= (+ quality 2) (:quality (inventory/age conjured-aged-brie))))))))
