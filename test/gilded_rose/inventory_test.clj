7a5c6c1c9dfa9f5440030ac0b8a8ee49850de57c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-05-06 13:37:50 +0200

    rename 'item-test' to 'inventory-test' inventory

--- test/gilded_rose/inventory_test.clj
+++ test/gilded_rose/inventory_test.clj
@@ -1,6 +1,6 @@
-(ns gilded-rose.item-test
+(ns gilded-rose.inventory-test
   (:require [clojure.test :refer :all]
-            [gilded-rose.inventory :as inventory]
+            [gilded-rose.inventory :refer :all]
             [gilded-rose.item :as item :refer [item]]))
 
 (def normal-item
@@ -40,51 +40,51 @@
     (testing
       "At the end of each day our system lowers sell-in value for an item"
       (let [sell-in (:sell-in normal-item)]
-        (is (= (dec sell-in) (:sell-in (inventory/age normal-item))))))
+        (is (= (dec sell-in) (:sell-in (age normal-item))))))
     (testing
       "At the end of each day our system lowers quality value for an item"
       (let [quality (:quality normal-item)]
-        (is (= (dec quality) (:quality (inventory/age normal-item))))))
+        (is (= (dec quality) (:quality (age normal-item))))))
     (testing
       "Once the sell by date has passed, quality degrades twice as fast"
       (let [quality (:quality sell-date-passed-item)]
-        (is (= (- quality 2) (:quality (inventory/age sell-date-passed-item))))))
+        (is (= (- quality 2) (:quality (age sell-date-passed-item))))))
     (testing
       "The quality of an item is never negative"
-      (is (= 0 (:quality (inventory/age zero-quality-item))))))
+      (is (= 0 (:quality (age zero-quality-item))))))
   (testing
     "Aged Brie"
     (testing
       "Increases in quality the older it gets"
       (let [quality (:quality standard-quality-aged-brie)]
-        (is (= (inc quality) (:quality (inventory/age standard-quality-aged-brie))))))
+        (is (= (inc quality) (:quality (age standard-quality-aged-brie))))))
     (testing
       "The quality is never more than 50"
-      (is (= 50 (:quality (inventory/age topped-quality-aged-brie))))))
+      (is (= 50 (:quality (age topped-quality-aged-brie))))))
   (testing
     "Sulfuras"
     (testing
       "Never has to be sold"
       (let [sell-in (:sell-in sulfuras)]
-        (is sell-in (:sell-in (inventory/age sulfuras)))))
+        (is sell-in (:sell-in (age sulfuras)))))
     (testing
       "Never decreases in quality"
-      (is 80 (:quality (inventory/age sulfuras)))))
+      (is 80 (:quality (age sulfuras)))))
   (testing
     "Backstage pass"
     (testing
       "Increases in quality as sell in value approaches"
       (let [quality (:quality far-future-backstage-pass)]
-        (is (= (inc quality) (:quality (inventory/age far-future-backstage-pass))))))
+        (is (= (inc quality) (:quality (age far-future-backstage-pass))))))
     (testing
       "Quality increases by 2 when sell in is 10 days or less"
       (let [quality (:quality near-future-backstage-pass)]
-        (is (= (+ quality 2) (:quality (inventory/age near-future-backstage-pass))))))
+        (is (= (+ quality 2) (:quality (age near-future-backstage-pass))))))
     (testing
       "Quality increases by 3 when sell in is 5 days or less"
       (let [quality (:quality immediate-future-backstage-pass)]
-        (is (= (+ quality 3) (:quality (inventory/age immediate-future-backstage-pass))))))
+        (is (= (+ quality 3) (:quality (age immediate-future-backstage-pass))))))
     (testing
       "Quality drops to 0 after the concert"
       (let [quality (:quality after-concert-backstage-pass)]
-        (is (= 0 (:quality (inventory/age after-concert-backstage-pass))))))))
+        (is (= 0 (:quality (age after-concert-backstage-pass))))))))
